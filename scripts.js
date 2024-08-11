1a48f7ae7043c2eec07f98e1c7680aa9478c785e
nxgmn <136024956+nxgmn@users.noreply.github.com>
2024-08-11 10:27:48 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/*
+    File: scripts.js
+    Author: Casey Chin
+
+    Creates classes in create.html and displays them in main.html
+*/
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
